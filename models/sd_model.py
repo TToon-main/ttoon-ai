ab14a28ce378c1edf0007e89fc0df80a8598f1cf
jhchoi316 <chlwogns316@gmail.com>
2024-06-26 17:25:14 +0900

    [REFACTOR] Modify sd_model.py

--- models/sd_model.py
+++ models/sd_model.py
@@ -1,5 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from configs import pipeline_config
-import embeddings
 from diffusers import StableDiffusionPipeline,DPMSolverSinglestepScheduler
 import transformers
 import torch
